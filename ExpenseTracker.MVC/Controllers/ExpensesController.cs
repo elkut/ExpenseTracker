c4dd3f489a8b5e76eb4a8efb57afb1f8e0d32248
Alkut Askar <elkut.esker@gmail.com>
2026-07-28 22:25:30 -0400

    Add project files.

---
+++ ExpenseTracker.MVC/Controllers/ExpensesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.MVC.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            return View();
+        }
+    }
+}
